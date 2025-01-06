4efa30ae2f21b87b5615afb0c3f8dd52b6d1bf5b
Aswad Khan <aswadkhan00786@gmail.com>
2025-01-06 22:58:01 +0500

    Refactor landing page layout and enhance navigation with a new NavBar component

--- src/app/(landingPage)/layout.tsx
+++ src/app/(landingPage)/layout.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
+import NavBar from "@/components/nav-bar";
 
-const LandingPage = async ({
+const LandingLayout = async ({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
@@ -13,8 +14,12 @@ const LandingPage = async ({
 	if (isUserAuthenticated) {
 		redirect("/dashboard");
 	}
-
-	return <div>{children}</div>;
+	return (
+		<div>
+			<NavBar />
+			{children}
+		</div>
+	);
 };
 
-export default LandingPage;
+export default LandingLayout;
